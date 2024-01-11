8d0d98ce3dd2c18c01c7c17d4626af0682989e4f
azeadrr <azeadn1@gmail.com>
2024-01-11 12:30:08 -0800

    mandatory tasks

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """string and int/float to tuple"""
-from typing import Union,Tuple
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
