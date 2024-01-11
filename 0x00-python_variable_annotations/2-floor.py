7207ebb89cc11167e8dacf60beda1912a2aad9d1
azeadrr <azeadn1@gmail.com>
2024-01-11 12:32:41 -0800

    mandatory tasks

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-"""2- floor"""
+"""2-floor"""
 
 
 def floor(n: float) -> int:
-    """function floor
-     which takes a float n as argument
-     returns the floor of the float
-     """
-     return int(n)
+    """floor function
+
+    Args:
+        n (float): number to floor
+
+    Returns:
+        int: floored number
+    """
+    return int(n)
