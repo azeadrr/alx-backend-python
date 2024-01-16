7e5103de36442ec34326ec23138c55e6d1388c2a
azeadrr <azeadn1@gmail.com>
2024-01-16 14:32:33 -0800

    mandatory tasks

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,6 @@ async_c = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """measure_runtime coroutine"""
-    cr = time.perf_cntr()
+    cr = time.perf_counter()
     await asyncio.gather(*list(it.repeat(async_c(), 4)))
-    return (time.perf_cntr() - cr)
+    return (time.perf_counter() - cr)
